3d4570a2fa9b32ecb531dceb004b48ce75b27a2c
satinderpal <esspee13@gmail.com>
2017-08-06 14:23:44 -0700

    report pro

---
+++ Report/src/test1.java
@@ -0,0 +1,27 @@
+
+import java.io.IOException;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import warehouse.Bhandle;
+
+
+public class test1 extends Bhandle {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver","c:/selenium/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://qaonair.com");
+		driver.manage().window().maximize();
+
+	driver.findElement(By.xpath("html/body/header/div/div/div[4]/div/ul/li[1]/a")).click();
+	driver.findElement(By.xpath(".//*[@id='signin_form']/div[1]/input")).sendKeys("hello");
+	driver.findElement(By.xpath("dfghjk")).sendKeys("dfghjk");
+		
+}
+	
+}
+
+
